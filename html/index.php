14c76bed36599814f6f37fa694d8ea81e1609090
Rena <ren484@mail.ru>
2025-01-20 14:40:34 +0400

    'edit_index_php'

--- html/index.php
+++ html/index.php
@@ -3,17 +3,17 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Список книг");
 
 if (CModule::IncludeModule("iblock") && CModule::IncludeModule("catalog")) {
-    $iblockId = 5; // Замените на ID вашего инфоблока книг
+    $iblockId = 5;
 
-    $arSelect = ["ID", "NAME"]; // Убираем свойства цен
+    $arSelect = ["ID", "NAME"];
     $arFilter = ["IBLOCK_ID" => $iblockId, "ACTIVE" => "Y"];
 
     $res = CIBlockElement::GetList(
-        ["SORT" => "ASC"], // Сортировка
-        $arFilter,         // Условия фильтрации
-        false,             // Группировка
-        ["nPageSize" => 5], // Пагинация
-        $arSelect          // Поля для выборки
+        ["SORT" => "ASC"],
+        $arFilter,
+        false,
+        ["nPageSize" => 5],
+        $arSelect
     );
 
     while ($arItem = $res->GetNext()) {
@@ -27,28 +27,28 @@ if (CModule::IncludeModule("iblock") && CModule::IncludeModule("catalog")) {
         ]);
 
         while ($price = $priceRes->fetch()) {
-            if ($price['CATALOG_GROUP_ID'] == 1) { // ID базовой цены
+            if ($price['CATALOG_GROUP_ID'] == 1) {
                 $priceBase = $price['PRICE'];
-            } elseif ($price['CATALOG_GROUP_ID'] == 2) { // ID дополнительной цены
+            } elseif ($price['CATALOG_GROUP_ID'] == 2) {
                 $priceSale = $price['PRICE'];
             }
         }
 
-        // Рассчитываем итоговую цену
+
         $finalPrice = $priceSale !== null ? min($priceBase, $priceSale) : $priceBase;
 
-        // Получение всех авторов книги
+
         $authors = [];
         $propertyRes = CIBlockElement::GetProperty(
             $iblockId,
             $arItem["ID"],
             [],
-            ["CODE" => "AUTHORS"] // Получаем только свойство AUTHORS
+            ["CODE" => "AUTHORS"]
         );
 
         while ($property = $propertyRes->Fetch()) {
             if ($property["VALUE"]) {
-                // Запрос к инфоблоку авторов для получения имени
+
                 $authorRes = CIBlockElement::GetList(
                     [],
                     ["ID" => $property["VALUE"]],
