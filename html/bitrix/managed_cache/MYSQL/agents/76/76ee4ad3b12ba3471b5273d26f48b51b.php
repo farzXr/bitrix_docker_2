947d931996ee9b1fa5c76012ae9795c75e5423d8
Rena <ren484@mail.ru>
2025-01-20 15:23:32 +0400

    'edit_css_2'

--- html/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ html/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001737371743';
-$dateexpire = '001737375343';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1737371792;}';
+$datecreate = '001737372194';
+$dateexpire = '001737375794';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1737372241;}';
 return true;
 ?>
--- html/bitrix/managed_cache/MYSQL/b_option/05/05a4b854c735508fd5190cef185874b0.php
+++ html/bitrix/managed_cache/MYSQL/b_option/05/05a4b854c735508fd5190cef185874b0.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001737312807';
-$dateexpire = '001737316407';
+$datecreate = '001737372188';
+$dateexpire = '001737375788';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:0:{}}}';
 return true;
 ?>
